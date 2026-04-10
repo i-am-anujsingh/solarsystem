78972e702e844871097af56a5f08355e92f8d1ec
Sanjay Joshi <joshisanjay9335@gmail.com>
2026-04-10 21:03:08 +0530

    Project Done

--- planets/model.js
+++ planets/model.js
@@ -1,6 +1,5 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.150.1/examples/jsm/loaders/GLTFLoader.js";
-
 const scene = new THREE.Scene();
 let solarsystem;
 let mixer;
